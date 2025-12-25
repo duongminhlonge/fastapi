44a6044049d3e4aba3d877d5ca949d6be38020b4
Long Duong <duongminhlonge@gmail.com>
2025-12-25 17:49:51 +0700

    How to use debug

--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 from app.api.v1 import routes as v1_routes
+import uvicorn
+
 
 app = FastAPI(title="API Example")
 app.include_router(v1_routes.router, prefix="/v1")
+
+# Add this to use debug (before run debug please don't run uvicorn app.main:app --reload)
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=False)
