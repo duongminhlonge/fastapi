cdeb77b276dcf4315af41da3e02ddb97df296759
Long Duong <duongminhlonge@gmail.com>
2025-12-27 15:58:08 +0700

    Update

--- app/main.py
+++ app/main.py
@@ -8,4 +8,4 @@ app.include_router(v1_routes.router, prefix="/v1")
 
 # Add this to use debug (before run debug please don't run uvicorn app.main:app --reload)
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=False)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=False)
